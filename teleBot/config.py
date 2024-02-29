20566f57a05731023ecef3e4a1f1b225c4c56c91
Name <email@example.ru>
2024-02-29 22:45:00 +0300

    Бот для телеграма

---
+++ teleBot/config.py
@@ -0,0 +1,11 @@
+TOKEN = '7187223225:AAHBDt83JCow6-q6Obd5owb_atQWLQ9UzRA'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'гривна': 'UAH',
+    'фунт': 'GBP'
+}
